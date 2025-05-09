75a5cb6ebc3e5e58314e75917c8dc7e73e8a3773
Achmad Dinofaldi Firmansyah <aldi.novaldi.frmnsyah@gmail.com>
2025-05-09 16:34:50 +0700

    feat: add assign assessment page and update routing

--- iconist_app/urls/configuration_urls.py
+++ iconist_app/urls/configuration_urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from ..views import configuration_views as views
 
 urlpatterns = [
-    path('create-question/', views.create_question, name='create_question')
+    path('create-question/', views.create_question, name='create_question'),
+    path('assign-assessment/', views.assign_assessment, name='assign_assessment'),
 ]
--- iconist_app/views/configuration_views.py
+++ iconist_app/views/configuration_views.py
@@ -6,3 +6,10 @@ def create_question(request):
     }
 
   return render(request, "konfigurasi/buat_soal.html", context)
+
+def assign_assessment(request):
+  context = {
+        'active_page': 'home',
+    }
+
+  return render(request, "konfigurasi/assign_assesment.html", context)
