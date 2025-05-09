434580294d2b384bc3ff90b2f573c1c3450c5f3d
Achmad Dinofaldi Firmansyah <aldi.novaldi.frmnsyah@gmail.com>
2025-05-09 17:11:45 +0700

    feat: add employee history and detail views, update templates and routing

--- iconist_app/urls/configuration_urls.py
+++ iconist_app/urls/configuration_urls.py
@@ -4,4 +4,6 @@ from ..views import configuration_views as views
 urlpatterns = [
     path('create-question/', views.create_question, name='create_question'),
     path('assign-assessment/', views.assign_assessment, name='assign_assessment'),
+    path('history/', views.history, name='configuration_history'),
+    path('history/employee/', views.employee, name='configuration_employee'),
 ]
